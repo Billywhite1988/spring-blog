82e06a2d245831e044a7b3b466c2db1a061e6c9b
Billy <billy_white.88@yahoo.com>
2018-04-17 12:27:24 -0500

    added Post Controller, post sub directory, and roll-dice html

--- src/main/java/com/example/blog/controllers/HomeController.java
+++ src/main/java/com/example/blog/controllers/HomeController.java
@@ -1,16 +1,23 @@
 package com.example.blog.controllers;
 
-import org.springframework.stereotype.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class HomeController {
 
-    @GetMapping("/welcome")
-    @ResponseBody
-    public String WelcomeHome(){
-        return "This is the landing page!";
+    @GetMapping("/home")
+    public String welcome() {
+        return "home";
+    }
+
+    @GetMapping("/home/{name}")
+    public String sayHello(@PathVariable String name, Model model) {
+        model.addAttribute("name", name);
+        return "home";
     }
 }
+
